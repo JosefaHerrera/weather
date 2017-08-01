e1ba84994ed4e4a1dfa38655a9f0652a5f4150e2
JosefaHerrera <josefa@linuxero.cl>
2017-08-01 17:23:25 -0400

    api conectada

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-//mi API
+///mi API
 //Https://api.darksky.net/forecast/e16a38db6186f1d2cbbf839bfac86e55/37.8267,122.4233
 /************UBICACIÓN********/
 function buscar(){
@@ -21,3 +21,20 @@ var funcionError = function(error){
 }
 
 /*************************/
+function clima(latitud, longitud) {
+    $.ajax({
+            url: 'https://api.darksky.net/forecast/e16a38db6186f1d2cbbf839bfac86e55/'+latitud+','+longitud'',
+            type: 'GET',
+            datatype: 'JSONP',
+        })
+        .done(function(response) {
+            console.log('prueba');
+            console.log(response);
+        })
+        .fail(function() {
+            console.log('error')
+        })
+        .always(function() {
+            console.log('complete')
+        });
+};
--- dist/js/script.js
+++ dist/js/script.js
@@ -12630,23 +12630,43 @@ if (typeof jQuery === 'undefined') {
 
 }(jQuery);
 
+///mi API
 //Https://api.darksky.net/forecast/e16a38db6186f1d2cbbf839bfac86e55/37.8267,122.4233
 /************UBICACIÓN********/
 function buscar(){
 	if(navigator.geolocation){
-		navigator.geolocation.getCurrentPosition(found, notFound);
+		navigator.geolocation.getCurrentPosition(funcionExito,funcionError );
 	}
 }
 window.addEventListener("load", buscar); 
 
 var latitud, longitud;
-var found = function(posicion){
+var funcionExito = function(posicion){
 	latitud = posicion.coords.latitude;
 	longitud = posicion.coords.longitude;
 	console.log(latitud);
 	console.log(longitud);
 	clima(latitud,longitud);
 }
-var notFound = function(error){
+var funcionError = function(error){
 	alert("ubicación no encontrada");
 }
+
+/*************************/
+function clima(latitud, longitud) {
+    $.ajax({
+            url: 'https://api.darksky.net/forecast/e16a38db6186f1d2cbbf839bfac86e55/'+latitud+','+longitud'',
+            type: 'GET',
+            datatype: 'JSONP',
+        })
+        .done(function(response) {
+            console.log('prueba');
+            console.log(response);
+        })
+        .fail(function() {
+            console.log('error')
+        })
+        .always(function() {
+            console.log('complete')
+        });
+};
