da86b61c4d82b17a855527b10d728d0b72a683e1
JosefaHerrera <josefa@linuxero.cl>
2017-08-01 21:20:10 -0400

    haciendo segunda pagina

--- assets/js/main.js
+++ assets/js/main.js
@@ -23,13 +23,17 @@ var funcionError = function(error){
 /*************************/
 function clima(latitud, longitud) {
     $.ajax({
-            url: 'https://api.darksky.net/forecast/e16a38db6186f1d2cbbf839bfac86e55/'+latitud+','+longitud'',
+            url: 'https://api.darksky.net/forecast/e16a38db6186f1d2cbbf839bfac86e55/'+latitud+','+longitud+'?anguage=es?&units=auto',
             type: 'GET',
-            datatype: 'JSONP',
+            datatype: 'JSON',
         })
         .done(function(response) {
-            console.log('prueba');
             console.log(response);
+            //currently = actualmente index.html
+            $('#climas').append('<span>La temperatura actual es: '+response.currently.temperature+'</span>' + '<br>'+'<span>La Humedad actual es: '+response.currently.humidity+'</span>' + '<br>'+'<span>Indice UV actual es: '+response.currently.uvIndex+'</span>' + '<br>'+'<span>La presión actual es: '+response.currently.pressure+'</span>' + '<br>'+'<a href=index2.html><button type="button" class="btn btn-default">Default</button></a>');
+
+            //daily , diario // semanal index2.html
+            $('#semanal').append('<span>La temperatura actual es: '+response.currently.temperature+'</span>' + '<br>'+'<span>El viento actual es: '+response.currently.windSpeed+'</span>' + '<br>'+'<span>La Humedad actual es: '+response.currently.humidity+'</span>' + '<br>'+'<span>Indice UV actual es: '+response.currently.uvIndex+'</span>' + '<br>'+'<span>La presión actual es: '+response.currently.pressure+'</span>' + '<br>'+'<a href=index2.html><button type="button" class="btn btn-default">Default</button></a>');
         })
         .fail(function() {
             console.log('error')
